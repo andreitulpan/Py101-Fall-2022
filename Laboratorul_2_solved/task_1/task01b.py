931c3cfb47edfebe3d0b23db7e892ed9fd45ecb7
Stefanita Ionita <72273318+cyber-stefik@users.noreply.github.com>
2022-10-30 20:17:49 +0200

    Update task01b.py

--- Laboratorul_2_solved/task_1/task01b.py
+++ Laboratorul_2_solved/task_1/task01b.py
@@ -10,10 +10,10 @@ def task1b(phrase):
     new_phrase = ""
     ################### TO DO #########################
     # folosim un string pentru a cunoaste vocalele, putem de asemenea sa folosim
-        # si o lista pentru vocale.
+        # si o lista pentru vocale
     vocale = "aeiou"
     # mapam functia lambda, care poate fi scrisa si ca parametru, nu neaparat
-        # ca o variabila.
+        # ca o variabila
     new_phrase = map(lambda x: x.upper() if x.lower() in vocale else x, phrase)
 
     ###################################################
