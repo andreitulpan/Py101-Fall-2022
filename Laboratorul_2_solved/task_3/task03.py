8f68433ba36352cd35f6af860bd6948280dcbbf0
Stefanita Ionita <72273318+cyber-stefik@users.noreply.github.com>
2022-10-30 20:19:13 +0200

    Update task03.py

--- Laboratorul_2_solved/task_3/task03.py
+++ Laboratorul_2_solved/task_3/task03.py
@@ -6,12 +6,12 @@ def task(register):
     names = []
 
     ################### TO DO #########################
-    # filtram elementele dictionarului folosind o lambda care ne calculeaza media.
+    # filtram elementele dictionarului folosind o functie lambda care ne calculeaza media
     # x[0] este cheia unui item, iar x[1] este valoarea unui item, in cazul nostru
-        # lista care contine notele.
+        # lista care contine notele
     students = list(filter(lambda x: (sum(x[1]) / len(x[1])) >= 8.5, register.items()))
 
-    # adaugam in lista finala numele studentilor ce au indeplinit conditia.
+    # adaugam in lista finala numele studentilor ce au indeplinit conditia
     for stud in students:
         names.append(stud[0])
     ###################################################
