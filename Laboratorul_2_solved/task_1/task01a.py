75d221b6f17ba407f40435fb71ff96641c45e1f7
Stefanita Ionita <72273318+cyber-stefik@users.noreply.github.com>
2022-10-30 20:17:02 +0200

    Update task01a.py

--- Laboratorul_2_solved/task_1/task01a.py
+++ Laboratorul_2_solved/task_1/task01a.py
@@ -14,11 +14,11 @@ def task1a(nums):
 
     ################### TO DO #########################
     # o functie lambda care dubleaza un element daca e divizibil cu 2,
-        # alftel il triplam.
+        # alftel il triplam
     doubleElem = lambda x : x * 2 if x % 6 == 0 else x * 3
-    # mapam functia lambda pentru fiecare element din nums.
+    # mapam functia lambda pentru fiecare element din nums
     # va puteti imagina ca map face un for prin elementele lui nums si aplica functia doubleElem
-        # pe fiecare element din nums.
+        # pe fiecare element din nums
     result = (map(doubleElem, nums))
     ###################################################
 
